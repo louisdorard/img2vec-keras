cc1f030a01375b6da38209c470fdbc4c900c18eb
Louis Dorard <louis@louisdorard.com>
2020-06-03 12:52:28 +0200

    Break main function down into subfunctions

--- img2vec_keras/img_to_vec.py
+++ img2vec_keras/img_to_vec.py
@@ -9,9 +9,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Model
 import numpy as np
 
-
 _IMAGE_NET_TARGET_SIZE = (224, 224)
-
 
 class Img2Vec(object):
 
@@ -23,28 +21,41 @@ class Img2Vec(object):
             self.preprocess_fn = mobilenet_v2.preprocess_input
         else:
             print("Using ResNet50 to vectorize images")
-        model = resnet50.ResNet50(weights='imagenet')
-        layer_name = 'avg_pool'
+            model = resnet50.ResNet50(weights='imagenet')
+            layer_name = 'avg_pool'
             self.preprocess_fn = resnet50.preprocess_input
         o = model.get_layer(layer_name).output
         self.vec_len = o.shape[1]
         self.intermediate_layer_model = Model(inputs=model.input, 
-                                              outputs=model.get_layer(layer_name).output)
+                                              outputs=o)
 
+    def path2img(self, image_path):
+        return image.load_img(image_path, target_size=_IMAGE_NET_TARGET_SIZE)
 
-    def get_vec(self, image_path):
-        """ Gets a vector embedding from an image.
+    def img_to_array(self, img):
+        return image.img_to_array(img)
+
+    def img_preprocess(self, img):
+        img_processed = img.copy()
+        # if we're given just one image, we need to expand dims; otherwise we go straight to preprocessing
+        if len(img_processed.shape) < 4:
+            img_processed = np.expand_dims(img_processed, axis=0)
+        img_processed = self.preprocess_fn(img_processed)
+        return img_processed
+
+    def img_processed_to_vec(self, img_processed):
+        intermediate_output = self.intermediate_layer_model.predict(img_processed)
+        return intermediate_output
+
+    def img2vec(self, img):
+        return self.img_processed_to_vec(self.img_preprocess(self.img_to_array(img)))
+
+    def path2vec(self, image_path):
+        """ Gets a vector embedding from an image, given the path to the image file.
         :param image_path: path to image on filesystem
         :returns: numpy ndarray
         """
+        return self.img2vec(self.path2img(image_path))
 
-        img = image.load_img(image_path, target_size=_IMAGE_NET_TARGET_SIZE)
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = resnet50.preprocess_input(x)
-        intermediate_output = self.intermediate_layer_model.predict(x)
-        
-        return intermediate_output[0]
-    
 if __name__ == "main":
      pass    
